e4c4c0072a5e8bbb1642bf337c73d8ffc3d5c2e2
felipy <felipy81br@gmail.com>
2025-05-25 17:51:07 -0300

    feat(schedule): implement date selection and confirmation flow in ScheduleForm

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,22 @@
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null)
+
+  function handleClearSelectdDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectdDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
