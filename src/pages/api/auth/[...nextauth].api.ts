02c31c4870c7007a6690dede4190d8320340551d
felipy <felipy81br@gmail.com>
2025-05-25 20:42:25 -0300

    feat: integrate Google OAuth with token refresh mechanism
    - Added googleapis package to handle Google OAuth.
    - Implemented getGoogleOAuthToken function to manage access token renewal.
    - Updated NextAuth configuration to request offline access for refresh token.

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -17,6 +17,9 @@ export function buildNextAuthOptions(
           params: {
             scope:
               'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
           },
         },
         profile(profile: GoogleProfile) {
