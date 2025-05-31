06b709881ff242d75d246f0b27f891c23325d44d
felipy <felipy81br@gmail.com>
2025-05-31 20:27:04 -0300

    feat: add next-seo for improved SEO management and update home page metadata

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,28 +3,35 @@ import { Container, Hero, Preview } from './styles'
 import Image from 'next/image'
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as="h1" size="4xl">
-          Agendamento descomplicado
-        </Heading>
-        <Text size="lg">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          quality={100}
-          priority
-          alt="Preview do aplicativo Ignite Call"
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading as="h1" size="4xl">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="lg">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            quality={100}
+            priority
+            alt="Preview do aplicativo Ignite Call"
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
